c81caeae77b177feca08ce03392f9191fcdf886e
Utsav Chokshi <utsav.chokshi@codenation.co.in>
2023-12-06 08:01:49 +0000

    Add time calculation

--- transcript.py
+++ transcript.py
@@ -4,6 +4,7 @@ from deepgram import Deepgram
 import asyncio, json
 import os
 import sys
+import time
 
 # Your Deepgram API Key
 DEEPGRAM_API_KEY = os.environ["DEEPGRAM_API_KEY"]
@@ -17,6 +18,7 @@ FILE = sys.argv[1]
 # Include this line only if transcribing a local file
 # Example: audio/wav
 MIMETYPE = 'audio/wav'
+
 
 async def main():
 
@@ -55,7 +57,7 @@ async def main():
 
   # Write the response to the console
   # print(json.dumps(response, indent=4))
-  
+
   deepgram.extra.to_SRT(response)
 
 
@@ -65,7 +67,13 @@ async def main():
 try:
   # If running in a Jupyter notebook, Jupyter is already running an event loop, so run main with this line instead:
   #await main()
+  start_time = time.time()
   asyncio.run(main())
+  end_time = time.time()
+  execution_time = end_time - start_time
+  print(
+      f"It took {execution_time:.3f} seconds to execute"
+  )
 except Exception as e:
   exception_type, exception_object, exception_traceback = sys.exc_info()
   line_number = exception_traceback.tb_lineno
